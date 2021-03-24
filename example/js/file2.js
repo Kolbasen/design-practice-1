40bc599ed9a2beca9dd8be8cb6ae43418f3ad42f
aleksandr-kuharenko <oleksandr.kukharenko@binary-studio.com>
2021-03-24 18:10:30 +0200

    feat: * add one more js file * fix build issue * run both tests and build if main file changed * run only tests if test file changed * fix OBFUSCATE flag * refactor

---
+++ example/js/file2.js
@@ -0,0 +1,7 @@
+function returnTwo() {
+    console.log('Hello from function returnTwo')
+    return 2;
+}
+
+const result = returnTwo();
+console.log(result);
--- example/js/webpack.config.js
+++ example/js/webpack.config.js
@@ -2,12 +2,10 @@ const path = require('path');
 const WebpackObfuscator = require('webpack-obfuscator');
 
 module.exports = (env = {}) => {
-
-    console.log(env)
-
     const { ENTRY, SHOULD_OBFUSCATE, FILENAME = 'index.js' } = env;
+    const OBFUSCATE = SHOULD_OBFUSCATE === 'true';
 
-    const obfuscatePlugin = Boolean(SHOULD_OBFUSCATE) ?
+    const obfuscatePlugin = OBFUSCATE ?
         [
             new WebpackObfuscator ({
                 rotateStringArray: true
@@ -15,13 +13,12 @@ module.exports = (env = {}) => {
         ]
         : []
 
-    console.log(obfuscatePlugin);
 
     return {
         mode: 'production',
         entry: ENTRY.split(','),
         output: {
-            path: path.resolve(__dirname),
+            path: path.resolve(__dirname + `/../out/js/`),
             filename: `${FILENAME}.js`,
           },
         plugins: obfuscatePlugin,
